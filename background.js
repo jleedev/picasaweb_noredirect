43da4d57ec6aa467db2a5939f557608d2e08f095
Josh Lee <jleedev@gmail.com>
2012-03-30 15:52:31 -0500

    Initial commit

---
+++ background.js
@@ -0,0 +1,9 @@
+chrome.cookies.set({
+    url: 'https://picasaweb.google.com',
+    name: 'noRedirect',
+    value: '1',
+    domain: 'picasaweb.google.com',
+    path: '/',
+    secure: true,
+    httpOnly: true,
+    storeId: "0"});
